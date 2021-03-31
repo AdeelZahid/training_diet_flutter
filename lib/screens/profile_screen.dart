2ecf930bfe9df93be28cbb532afb35917cf992cb
AdeelZahid <m.adeel2144@gmail.com>
2021-03-31 20:03:36 +0500

    Hello User widget is done.

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:training_diet/model/meal.dart';
 import 'package:training_diet/widgets/mealcard.dart';
+import 'package:vector_math/vector_math_64.dart' as math;
+import 'package:intl/intl.dart';
+import 'package:animations/animations.dart';
 
 class ProfileScreen extends StatefulWidget {
-  Meal meals;
+  final Meal meals;
+
+  const ProfileScreen({Key key, this.meals}) : super(key: key);
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
@@ -27,6 +32,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final today = DateTime.now();
 
     return Scaffold(
       backgroundColor: const Color(0xFFE9E9E9),
@@ -39,10 +46,85 @@ class _ProfileScreenState extends State<ProfileScreen> {
             right: 0,
             child: ClipRRect(
               borderRadius: const BorderRadius.vertical(
-                bottom: Radius.circular(40.0),
+                bottom: const Radius.circular(40),
               ),
               child: Container(
                 color: Colors.white,
+                padding: const EdgeInsets.only(
+                    top: 30, left: 32, right: 16, bottom: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        "${DateFormat("EEEE").format(today)}, ${DateFormat("d MMMM").format(today)}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "Hello, Adeel",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 26,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: ClipOval(
+                          child: Image.asset("assets/images/userr.jpg")),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        _RadialProgress(
+                          width: width * 0.3,
+                          height: width * 0.3,
+                          progress: 0.7,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            _IngredientProgress(
+                              ingredient: "Protein",
+                              progress: 0.3,
+                              progressColor: Colors.green,
+                              leftAmount: 72,
+                              width: width * 0.28,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            _IngredientProgress(
+                              ingredient: "Carbs",
+                              progress: 0.2,
+                              progressColor: Colors.red,
+                              leftAmount: 252,
+                              width: width * 0.28,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            _IngredientProgress(
+                              ingredient: "Fat",
+                              progress: 0.1,
+                              progressColor: Colors.yellow,
+                              leftAmount: 61,
+                              width: width * 0.28,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -226,5 +308,144 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ),
       ),
     );
+  }
+}
+
+class _IngredientProgress extends StatelessWidget {
+  final String ingredient;
+  final int leftAmount;
+  final double progress, width;
+  final Color progressColor;
+
+  const _IngredientProgress(
+      {Key key,
+      this.ingredient,
+      this.leftAmount,
+      this.progress,
+      this.progressColor,
+      this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          ingredient.toUpperCase(),
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 10,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: Colors.black12,
+                  ),
+                ),
+                Container(
+                  height: 10,
+                  width: width * progress,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    color: progressColor,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text("${leftAmount}g left"),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _RadialProgress extends StatelessWidget {
+  final double height, width, progress;
+
+  const _RadialProgress({Key key, this.height, this.width, this.progress})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _RadialPainter(
+        progress: 0.7,
+      ),
+      child: Container(
+        height: height,
+        width: width,
+        child: Center(
+          child: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "1731",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: const Color(0xFF200087),
+                  ),
+                ),
+                TextSpan(text: "\n"),
+                TextSpan(
+                  text: "kcal left",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: const Color(0xFF200087),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RadialPainter extends CustomPainter {
+  final double progress;
+
+  _RadialPainter({this.progress});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..strokeWidth = 10
+      ..color = Color(0xFF200087)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double relativeProgress = 360 * progress;
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: size.width / 2),
+      math.radians(-90),
+      math.radians(-relativeProgress),
+      false,
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
